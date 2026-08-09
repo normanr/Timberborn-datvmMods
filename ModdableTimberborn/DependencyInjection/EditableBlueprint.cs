a5ab774385d29a095645ebf3cb4843c81d7fcc5b
Norman Rasmussen <norman@rasmussen.co.za>
2026-08-08 21:31:33 -0700

    Propagate and supplement blueprint source information when editing

--- ModdableTimberborn/DependencyInjection/EditableBlueprint.cs
+++ ModdableTimberborn/DependencyInjection/EditableBlueprint.cs
@@ -6,16 +6,29 @@ public class EditableBlueprint(string name)
     public string Name { get; set; } = name;
     public List<EditableBlueprint> Children { get; set; } = [];
     public List<ComponentSpec> Specs { get; set; } = [];
+    BlueprintFileBundle? source;
 
     public EditableBlueprint(Blueprint blueprint) : this(blueprint.Name)
     {
         Children = [.. blueprint.Children.Select(static q => new EditableBlueprint(q))];
         Specs = [.. blueprint.Specs];
+        try {
+            source = ContainerRetriever.GetInstance<BlueprintSourceService>().Get(blueprint);
+        }
+        catch
+        {
+        }
     }
 
     public EditableBlueprint(string name, ComponentSpec spec) : this(name)
     {
         Specs.Add(spec);
+        try {
+            source = ContainerRetriever.GetInstance<BlueprintSourceService>().Get(spec.Blueprint);
+        }
+        catch
+        {
+        }
     }
 
     public void TransformSpecs(Func<ComponentSpec, ComponentSpec?> transformer)
@@ -26,6 +39,10 @@ public class EditableBlueprint(string name)
             if (modified is not null)
             {
                 Specs[i] = modified;
+                if (source != null)
+                {
+                    source = source.AddJson("{\"" + Specs[i].GetType().Name + "\": {}}", transformer.Method.DeclaringType.Name + "." + transformer.Method.Name);
+                }
             }
         }
     }
@@ -35,7 +52,15 @@ public class EditableBlueprint(string name)
 
     public T GetSpec<T>() where T : ComponentSpec => Specs.OfType<T>().First();
 
-    public Blueprint ToBlueprint() => new(Name, Specs, [.. Children.Select(static q => q.ToBlueprint())]);
+    public Blueprint ToBlueprint()
+    {
+        Blueprint blueprint = new(Name, Specs, [.. Children.Select(static q => q.ToBlueprint())]);
+        if (source != null)
+        {
+            ContainerRetriever.GetInstance<BlueprintSourceService>().Add(blueprint, source);
+        }
+        return blueprint;
+    }
     public static implicit operator Blueprint(EditableBlueprint editableBlueprint) => editableBlueprint.ToBlueprint();
 
 }
